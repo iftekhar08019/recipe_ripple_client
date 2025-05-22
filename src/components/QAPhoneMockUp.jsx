925956784b326e6baf5da823b6311212cdcdf231
iftekhar08019 <iftekhar08019@gmail.com>
2025-05-22 21:18:03 +0200

    FAQ section added

---
+++ src/components/QAPhoneMockUp.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+const QAPhoneMockup = () => {
+  return (
+    <>
+      <h1 className="lg:text-4xl text-xl font-bold text-center mt-15">
+        Frequently Asked Questions
+      </h1>
+      <section className="flex flex-col md:flex-row items-center justify-between max-w-6xl mx-auto p-6 md:p-12 gap-10">
+        {/* Left side: Q&A */}
+        <div className="flex-1 text-center md:text-left space-y-6">
+          <div className="space-y-4 text-gray-700">
+            <div>
+              <h3 className="font-semibold text-xl">What is Recipe Ripple?</h3>
+              <p>
+                Recipe Ripple is a platform where you can find and share amazing
+                recipes from around the world.
+              </p>
+            </div>
+            <div>
+              <h3 className="font-semibold text-xl">
+                How do I submit a recipe?
+              </h3>
+              <p>
+                You can submit recipes by registering and using the "Add Recipe"
+                feature in your dashboard.
+              </p>
+            </div>
+            <div>
+              <h3 className="font-semibold text-xl">
+                Can I save my favorite recipes?
+              </h3>
+              <p>
+                Yes! You can like and save recipes to your profile for easy
+                access later.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Right side: Phone mockup */}
+    
+        <div className="flex-1 flex justify-center md:justify-end hidden lg:block">
+            
+          <div className="mockup-phone border-primary ">
+            <div className="mockup-phone-camera"></div>
+            <div className="mockup-phone-display bg-[#fbfbfb] flex flex-col justify-center items-center">
+                <h1 className="text-2xl text-center">Come and Cook <br />With <br />Recipe Ripple</h1>
+              <img
+                alt="wallpaper"
+                src="https://cdn.dribbble.com/userupload/27165894/file/original-0a90d009f3864792265734f1ddfed3a5.gif"
+               
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default QAPhoneMockup;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import HeroSlider from '../components/Hero';
 import RecipeCard from '../components/RecipeCard';
 import TopRecipe from '../components/TopRecipe';
 import StatSection from '../components/StatSection';
+import QAPhoneMockup from '../components/QAPhoneMockUp';
 
 const Home = () => {
     return (
         <div>
             <HeroSlider></HeroSlider>
             <TopRecipe></TopRecipe>
+            <QAPhoneMockup></QAPhoneMockup>
             <StatSection></StatSection>
         </div>
     );
