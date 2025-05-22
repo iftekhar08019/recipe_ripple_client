a7aa41ba5003909d4f57bb5ebcbd372c54f60aba
iftekhar08019 <iftekhar08019@gmail.com>
2025-05-22 20:52:01 +0200

    stat section added

--- src/components/TopRecipe.jsx
+++ src/components/TopRecipe.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import RecipeCard from "./RecipeCard";
+import { Link } from "react-router";
 
 const TopRecipe = () => {
   const [recipes, setRecipes] = useState([]);
@@ -17,11 +18,20 @@ const TopRecipe = () => {
 
   return (
     <>
-     <h1 className="lg:text-4xl text-xl font-bold text-center mt-15">Top Recipes</h1>
-      <div className="lg:w-10/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {recipes.map((recipe) => (
-          <RecipeCard key={recipe.id} recipe={recipe} />
-        ))}
+      <div className="flex flex-col items-center">
+        <h1 className="lg:text-4xl text-xl font-bold text-center mt-15">
+          Top Recipes
+        </h1>
+        <div className="lg:w-10/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {recipes.map((recipe) => (
+            <RecipeCard key={recipe.id} recipe={recipe} />
+          ))}
+        </div>
+        <div className="card-actions mt-10">
+          <Link to="/recipes" className="btn btn-primary">
+            View All Recipes
+          </Link>
+        </div>
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import HeroSlider from '../components/Hero';
 import RecipeCard from '../components/RecipeCard';
 import TopRecipe from '../components/TopRecipe';
+import StatSection from '../components/StatSection';
 
 const Home = () => {
     return (
         <div>
             <HeroSlider></HeroSlider>
             <TopRecipe></TopRecipe>
+            <StatSection></StatSection>
         </div>
     );
 };
