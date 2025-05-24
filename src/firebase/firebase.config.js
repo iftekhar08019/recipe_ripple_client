553f09dca34d98ecc09c2bc17bbaa0cbfe1b7ecf
iftekhar08019 <iftekhar08019@gmail.com>
2025-05-24 14:10:58 +0200

    env file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,16 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDXwDsDWlQW6HCygInDLpyhPDt3ASDEmnM",
-  authDomain: "recipe-ripple.firebaseapp.com",
-  projectId: "recipe-ripple",
-  storageBucket: "recipe-ripple.firebasestorage.app",
-  messagingSenderId: "315251894945",
-  appId: "1:315251894945:web:cb7189e41c79131b30dee0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
 export default app;
